e4c82188d8cf4aa5256db11d4cc8cd9688d267a2
Cristina <workris96@gmail.com>
2021-10-17 13:05:35 +0100

    auth model & auth data access object done

---
+++ back/auth/auth.model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    passwd: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    motherlang: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    studlangs: {
+        type: Array,
+        required: true
+    },
+    //decks:{}
+}, {
+    timestamps: true
+});
+
+module.exports = userSchema;
