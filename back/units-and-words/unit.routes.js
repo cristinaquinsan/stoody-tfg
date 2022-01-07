0148549b9e4e40c262f4e3ea24b66ad505afc34f
Cristina <workris96@gmail.com>
2022-01-07 16:55:27 +0000

    fixed minor bug in update route. Needed a slash that accidently was deleted

--- back/units-and-words/unit.routes.js
+++ back/units-and-words/unit.routes.js
@@ -4,7 +4,7 @@ module.exports = (router) => {
     router.get('/unit/:id', Units.viewUnit);
     router.get('/myLanguages/:username/:language', Units.getUnitsByLanguage);
     router.post('/units', Units.newUnit);
-    router.put('/updateUnit:id', Units.editUnit);
+    router.put('/updateUnit/:id', Units.editUnit);
     router.put('/updateFeedback', Units.updateFeedback)
     router.delete('/units/:id',Units.deleteUnit);
 }
